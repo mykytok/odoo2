26535f1caa1986006bd862242ddedafba009a60b
Mykyta <nikita.ogirchuk@gmail.com>
2025-06-15 11:13:26 +0300

    task 3 20250615

--- hr_hospital/wizard/hr_hospital_persdoc_update_wzd.py
+++ hr_hospital/wizard/hr_hospital_persdoc_update_wzd.py
@@ -23,14 +23,17 @@ class PersonalDoctorUpdate(models.TransientModel):
     def default_get(self, vals):
         res = super().default_get(vals)
         if self.env.context.get('active_ids'):
-            active_patient_ids = self.env['hr.hospital.patient'].browse(self.env.context.get('active_ids'))
+            active_patient_ids = self.env['hr.hospital.patient'].browse(
+                self.env.context.get('active_ids'))
             res['patient_ids'] = [(6, 0, active_patient_ids.mapped('id'))]
         return res
 
     def update_personal_doctor(self):
         self.ensure_one()
         # Stable approach for white objects
-        self.patient_ids.write({'hr_hospital_personal_doctor_id': self.doctor_id})
+        self.patient_ids.write(
+            {'hr_hospital_personal_doctor_id': self.doctor_id}
+        )
         # Bad approach for white objects
         # for patient_id in self.patient_ids:
         #     patient_id.hr_hospital_personal_doctor_id = self.doctor_id
